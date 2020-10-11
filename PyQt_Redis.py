56a4999d20810040a4978ce8c4f9de97ba385231
bjtuer-studio <57830526+bjtuer-studio@users.noreply.github.com>
2020-10-11 13:38:05 +0800

    新增了点击和时间

--- PyQt_Redis.py
+++ PyQt_Redis.py
@@ -22,10 +22,11 @@ class Demo(QWidget):
         self.MyRedis.set("User1","0")
         self.MyRedis.set("User2","0")
         self.MyRedis.set("User3","0")
-        self.MyRedis.hset("User1_time","User1",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
-        self.MyRedis.hset("User2_time","User2",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
-        self.MyRedis.hset("User3_time","User3",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
+        # self.MyRedis.hset("User1_time","User1",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
+        # self.MyRedis.hset("User2_time","User2",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
+        # self.MyRedis.hset("User3_time","User3",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
         self.MyRedis.lpush("Click_Time",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
+        self.MyRedis.sadd("Click_Time_Hour", datetime.datetime.now().strftime('%H'))
         self.v_layout.addWidget(self.button)
         self.v_layout.addWidget(self.button1)
         self.v_layout.addWidget(self.button2)
@@ -35,15 +36,22 @@ class Demo(QWidget):
         self.MyRedis.incr("User1")
         self.MyRedis.hset("User1_time","User1",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
         self.MyRedis.lpush("Click_Time",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
+        self.MyRedis.sadd("Click_Time_Hour", datetime.datetime.now().strftime('%H'))
+
     def Mytest2(self):
         self.MyRedis.incr("User2")
         self.MyRedis.hset("User1_time","User1",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
         self.MyRedis.lpush("Click_Time",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
+        self.MyRedis.sadd("Click_Time_Hour", datetime.datetime.now().strftime('%H'))
+
     def Mytest3(self):
         self.MyRedis.incr("User3")
         self.MyRedis.hset("User1_time","User1",datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         self.MyRedis.lpush("Click_Time",datetime.datetime.now().strftime('%Y-%m-%d  %H:%M:%S'))
+        self.MyRedis.sadd("Click_Time_Hour", datetime.datetime.now().strftime('%H'))
+
     def Mytest_show(self):
+        print("-------------------------------------------------------")
         print("User1相关信息:")
         print(self.MyRedis.get("User1"))
         print(self.MyRedis.hgetall("User1_time"))
@@ -53,12 +61,15 @@ class Demo(QWidget):
         print("User2相关信息:")
         print(self.MyRedis.get("User2"))
         print(self.MyRedis.hgetall("User2_time"))
-        print("最近一次点击时间")
+        print("点击时间")
         print(self.MyRedis.lrange('Click_Time', 0, -1))
+        print("点击时间(小时)")
+        print(self.MyRedis.smembers("Click_Time_Hour"))
         print("-------------------------------------------------------")
 
 
 if __name__ == '__main__':
+    print(redis.__version__)
     app = QApplication(sys.argv)
     demo = Demo()
     demo.resize(200,200)
